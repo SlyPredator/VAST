e779fdd22302c88b12620b8b87f288abdc4fdb46
SlyPredator <navneethmahadevan@gmail.com>
2022-09-25 21:04:59 +0530

    create main CTk window

---
+++ ctk_login.py
@@ -0,0 +1,68 @@
+import tkinter
+import tkinter.messagebox
+import customtkinter
+from pathlib import Path
+
+ASSETS_PATH = Path(__file__).resolve().parent / "assets"
+
+
+customtkinter.set_appearance_mode(
+    "System"
+)  # Modes: "System" (standard), "Dark", "Light"
+customtkinter.set_default_color_theme(
+    "dark-blue"
+)  # Themes: "blue" (standard), "green", "dark-blue"
+
+
+class App(customtkinter.CTk):
+
+    WIDTH = 862
+    HEIGHT = 519
+
+    def __init__(self):
+        super().__init__()
+
+        self.title("VAST - Car Rentals")
+        self.geometry(f"{App.WIDTH}x{App.HEIGHT}")
+        self.protocol(
+            "WM_DELETE_WINDOW", self.on_closing
+        )  # call .on_closing() when app gets closed
+
+        # ============ create two frames ============
+
+        # configure grid layout (2x1)
+        self.grid_columnconfigure(1, weight=1)
+        self.grid_rowconfigure(0, weight=1)
+
+        self.frame_left = customtkinter.CTkFrame(
+            master=self, width=180, corner_radius=0
+        )
+        self.frame_left.grid(row=0, column=0, sticky="nswe")
+
+        self.frame_right = customtkinter.CTkFrame(master=self)
+        self.frame_right.grid(row=0, column=1, sticky="nswe", padx=20, pady=20)
+
+        self.frame_left.grid_rowconfigure(
+            0, minsize=10
+        )  # empty row with minsize as spacing
+        self.frame_left.grid_rowconfigure(5, weight=1)  # empty row as spacing
+        self.frame_left.grid_rowconfigure(
+            8, minsize=20
+        )  # empty row with minsize as spacing
+        self.frame_left.grid_rowconfigure(
+            11, minsize=10
+        )  # empty row with minsize as spacing
+
+    def button_event(self):
+        print("Button pressed")
+
+    def change_appearance_mode(self, new_appearance_mode):
+        customtkinter.set_appearance_mode(new_appearance_mode)
+
+    def on_closing(self, event=0):
+        self.destroy()
+
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
