d17cc5e688d58696f785be12a519d5e16da5ba37
SlyPredator <navneethmahadevan@gmail.com>
2022-09-27 16:55:37 +0530

    module-ify classes

---
+++ main.py
@@ -0,0 +1,19 @@
+import tkinter
+import tkinter.messagebox
+from pathlib import Path
+
+import customtkinter
+from PIL import Image
+from PIL import ImageTk as itk
+from window_classes import *
+
+customtkinter.set_appearance_mode(
+    "System"
+)  # Modes: "System" (standard), "Dark", "Light"
+customtkinter.set_default_color_theme(
+    "dark-blue"
+)  # Themes: "blue" (standard), "green", "dark-blue"
+
+if __name__ == "__main__":
+    app = Login_App()
+    app.mainloop()
