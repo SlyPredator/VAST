687b7f459db405fa9474ef5a834434ac9bdf0f8a
SlyPredator <navneethmahadevan@gmail.com>
2022-10-01 14:50:49 +0530

    add funcs

--- dbfile.py
+++ dbfile.py
@@ -1,9 +1,7 @@
 import mysql.connector
 
 mydb = mysql.connector.connect(
-    host="localhost", 
-    user="root", 
-    database="mydatabase"
+    host="localhost", user="root", password="", database="mydatabase"
 )
 
 mycursor = mydb.cursor()
@@ -14,5 +12,9 @@ def mcs():
         print(x)
 
 
-def mycursor_fetch():
+def mycursor_fetch_cust():
     mycursor.execute("select * from customers")
+
+
+def mycursor_fetch_admin():
+    mycursor.execute("select * from admins")
