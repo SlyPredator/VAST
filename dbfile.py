d02029a0c214d626021c2e28905bc5d88d5e1d60
SlyPredator <navneethmahadevan@gmail.com>
2022-10-01 13:43:27 +0530

    refactor: delete test.csv

--- dbfile.py
+++ dbfile.py
@@ -1,17 +1,18 @@
 import mysql.connector
 
 mydb = mysql.connector.connect(
-    host="localhost",
-    user="root",
+    host="localhost", 
+    user="root", 
     database="mydatabase"
 )
 
 mycursor = mydb.cursor()
 
+
 def mcs():
     for x in mycursor:
         print(x)
 
+
 def mycursor_fetch():
     mycursor.execute("select * from customers")
-
